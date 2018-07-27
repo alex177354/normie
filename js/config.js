1fdf3866218eaa3792667761b3032846025a0db3
alex177354 <alex177354@users.noreply.github.com>
2018-07-27 03:44:53 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -129,7 +129,7 @@ var l_musicRandom = true;
 
 // Music volume
 // Choose a value between 0 (quietest) and 100 (loudest)
-var l_musicVolume = 10;
+var l_musicVolume = 5;
 
 // Enable custom messages?
 // 'true' or 'false'
